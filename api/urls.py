437cec3b590affcacf8e04445856a58bd5464de8
JustNekoChris <103611606+JustNekoChris@users.noreply.github.com>
2024-12-05 10:22:46 -0800

    Extras (#18)
    * friend routes have more functionality
    
    * getfriends was updated

--- api/urls.py
+++ api/urls.py
@@ -10,19 +10,25 @@ urlpatterns = [
     path('create/poll/', views.createPoll),
     path('create/option/', views.createOption),
     path('create/vote/', views.createVote),
-    path('create/friend/', views.createFriend),
 
     path('delete/user/<int:id>/', views.deleteUser),
     path('delete/poll/<int:id>/', views.deletePoll),
-    path('delete/friend/<int:id1>/<int:id2>/', views.deleteFriend),
+    path('delete/friend/<int:id1>/<int:id2>/', views.unfriend),
 
     path('update/user/<int:id>/', views.updateUserDetails),
-    path('update/friend/<int:id1>/<int:id2>/<int:status>/', views.updateFriend),
 
     path('user/<int:id>/', views.getUserDetails),
     path('userexists/<int:id>/', views.userExists),
     path('getfriends/<int:id>/', views.getFriends),
     path('searchusers/<str:query>/', views.searchUsers),
+
+    path('friendrequest/<int:id1>/<int:id2>/', views.friendRequest),
+    path('acceptfriendrequest/<int:id1>/<int:id2>/', views.acceptFriendRequest),
+    path('rejectfriendrequest/<int:id1>/<int:id2>/', views.rejectFriendRequest),
+    path('undofriendrequest/<int:id1>/<int:id2>/', views.undoFriendRequest),
+    path('unfriend/<int:id1>/<int:id2>/', views.unfriend),
+    path('block/<int:id1>/<int:id2>/', views.block),
+    path('unblock/<int:id1>/<int:id2>/', views.unblock),
 
     path('polls/', views.getActivePolls),
 
